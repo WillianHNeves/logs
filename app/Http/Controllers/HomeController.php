74d74024da8b464145e9a2f08f224811be02ddd7
Willian Neves <willian.henriqueneves@gmail.com>
2022-07-20 21:39:14 -0300

    Subtração com log

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,6 +14,14 @@ class HomeController extends Controller
     public function Sub($num1, $num2)
     {
         $result = $num1 - $num2;
+        
+        logger()->debug( 'Sub feita',
+            [
+                'num1' => $num1, 
+                'num2' => $num2, 
+                'sub' => $result
+            ]
+        );
 
         return view('dashboard', compact('result'));
     }
